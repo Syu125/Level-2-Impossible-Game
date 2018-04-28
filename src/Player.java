46e1a7151b1a022c8bc3eac8f923b7c8f7247941
Syu125 <league@WTS14-4.attlocal.net>
2018-04-28 16:00:35 -0700

    WorkedOnGame
    -got the x and y variables of the block to work
    -need to figure out how to use the arrow keys to move the block

---
+++ src/Player.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+
+public class Player extends Component {
+	int X;
+	int Y;
+
+	public Player(int x, int y, int width, int length) {
+		X = x;
+		Y = y;
+	}
+
+	void draw(Graphics g) {
+		g.setColor(Color.blue);
+		g.fillRect(X, Y, 30, 30);
+	}
+
+}
